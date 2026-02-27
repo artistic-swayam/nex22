b1a1d8974803f88457b87688957e8d7f7c3e287b
Swayam <152770645+artistic-swayam@users.noreply.github.com>
2026-02-27 14:12:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ tl.to("#bar", {
         loader.style.display = "none";
         gsap.from(".home", {
             opacity: 0,
-            duration: 1.5,
+            duration: 4,
             ease: "power2.out"
         })
     }
